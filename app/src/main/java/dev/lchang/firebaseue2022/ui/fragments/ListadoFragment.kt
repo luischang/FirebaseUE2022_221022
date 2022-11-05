548663865fbabee0c6ec0a1e711dc0be64167bb4
Luis Chang <luis.chang@outlook.com>
2022-11-05 12:56:05 -0500

    Course List in RecyclerView

--- app/src/main/java/dev/lchang/firebaseue2022/ui/fragments/ListadoFragment.kt
+++ app/src/main/java/dev/lchang/firebaseue2022/ui/fragments/ListadoFragment.kt
@@ -1,11 +1,18 @@
 package dev.lchang.firebaseue2022.ui.fragments
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.firestore.DocumentChange
+import com.google.firebase.firestore.FirebaseFirestore
 import dev.lchang.firebaseue2022.R
+import dev.lchang.firebaseue2022.ui.fragments.adapter.CourseAdapter
+import dev.lchang.firebaseue2022.ui.fragments.model.CourseModel
 
 class ListadoFragment : Fragment() {
     override fun onCreateView(
@@ -13,6 +20,34 @@ class ListadoFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_listado, container, false)
+        val view: View =  inflater.inflate(R.layout.fragment_listado, container, false)
+        val db = FirebaseFirestore.getInstance()
+        val lstCourses: ArrayList<CourseModel> = ArrayList()
+        val rvCourse: RecyclerView = view.findViewById(R.id.rvCourse)
+
+        db.collection("courses")
+            .addSnapshotListener{snap,e->
+                if(e!=null){
+                    Log.e("Firebase error","Error al listar los cursos")
+                    return@addSnapshotListener
+                }
+
+                for(dc in  snap!!.documentChanges){
+                    when(dc.type){
+                        DocumentChange.Type.ADDED,
+                        DocumentChange.Type.MODIFIED,
+                        DocumentChange.Type.REMOVED -> {
+                            lstCourses.add(
+                                          CourseModel(dc.document.data["description"].toString(),
+                                           dc.document.data["score"].toString()))
+                        }
+                    }
+
+                }
+                rvCourse.adapter = CourseAdapter(lstCourses)
+                rvCourse.layoutManager = LinearLayoutManager(requireContext())
+
+            }
+        return view
     }
 }
